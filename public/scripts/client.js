ca917a78677aab8c9db8c876935159aed74a9885
Robert Cordray <robertcordray@gmail.com>
2017-08-18 14:39:37 -0500

    added owners to DB

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,9 +2,27 @@ console.log('js');
 
 var app = angular.module('PetHotelApp', []);
 
-app.controller('PetHotelController', ['$http', function($http){
+app.controller('PetHotelController', ['$http', function($http) {
     console.log('PetHotel controller loaded')
     var self = this;
+
+    self.newOwner = {};
+    // self.newOwner = function(firstname, lastname) {
+
+    // };
+
+    self.postNewOwner = function() {
+        console.log('post has been hit');
+
+        $http({
+            method: 'POST',
+            url: '/owner',
+            data: self.newOwner
+        }).then(function(response) {
+            console.log(response.data);
+            self.newOwner = {};
+        });
+    }
 
 
 }]);
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var port = 5000
+var owner = require('./server/routes/owner')
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.use('/owner', owner)
 
 app.listen(port, function() {
     console.log('listening on port', port);
